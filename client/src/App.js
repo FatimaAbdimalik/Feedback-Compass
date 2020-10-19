a1ef180c988f4606879bd4a9f133e21d4450b844
Leida_Gandy <leida.gandy@gmail.com>
2020-10-19 20:02:12 +0100

    Display regions

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,22 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
-import Homepage from "./components/Homepage"
+import Homepage from "./components/Homepage";
 import LandingPage from "./components/LandingPage";
-import StudentProfile from "./components/StudentProfile"
+import StudentProfile from "./components/StudentProfile";
+import Regions from "./components/Regions";
+import NorthWestCohort from "./components/NortWestCohort";
 
 export function App() {
-return (
-		<Router>
+  return (
+    <Router>
       <Route exact path="/" component={LandingPage} />
-    <Route  path="/login" component={Homepage} />
-    <Route path="/student" component={StudentProfile} />
-  </Router>	
-	)	
+      <Route path="/login" component={Homepage} />
+      <Route path="/student" component={StudentProfile} />
+      <Route path="/regions" component={Regions} />
+      {/* <Route path="/nortwest" component={NorthWestCohort} /> */}
+    </Router>
+  );
 }
 
 export default App;
