a2f187a49f52a545b720009d13b0e65bbd62dff7
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-19 09:23:47 +0100

    changed some endpoints

--- server/api.js
+++ server/api.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { connection } from "./db";
+import { Connection } from "./db";
 const router = new Router();
 router.get("/", (_, res, next) => {
-  connection.connect((err) => {
+  Connection.connect((err) => {
     if (err) {
       return next(err);
     }
@@ -15,27 +15,12 @@ router.post("/login", (req, res, next) => {
   const password = req.body.password;
 
   if (email && password) {
-    connection.query(
-      "select * from students where email = $1 and password = $2",
+    Connection.query(
+      "select * from users where email = $1 and password = $2",
       [email, password],
       (err, result) => {
         if (result.rowCount > 0) {
           return res.status(200).send(result.rows[0]);
-        } else {
-          connection.query(
-            "select * from mentors where email = $1 and password = $2",
-            [email, password],
-            (err, result2) => {
-              if (result2.rowCount > 0) {
-                return res.status(200).send(result2.rows[0]);
-              } else {
-                res.status(404).json({
-                  msg:
-                    "User not found, please enter a valid email and password!",
-                });
-              }
-            }
-          );
         }
       }
     );
@@ -45,8 +30,8 @@ router.post("/login", (req, res, next) => {
 router.get("/students/:id", (_, res, next) => {
   let studentId = Number(_.params.id);
 
-  connection.query(
-    "SELECT * FROM students WHERE id = $1",
+  Connection.query(
+    "SELECT * FROM users WHERE id = $1 and user_type = 'student'",
     [studentId],
     (err, result) => {
       if (err) {
@@ -61,7 +46,7 @@ router.get("/feedback/:student_id", (_, res, next) => {
   const studentId = Number(_.params.student_id);
   const query =
     "SELECT sent_date, title, body, response FROM feedbacktable WHERE student_id= $1";
-  connection.query(query, [studentId], (err, result) => {
+  Connection.query(query, [studentId], (err, result) => {
     if (err) {
       return next(err);
     }
@@ -72,38 +57,88 @@ router.get("/feedback/:student_id", (_, res, next) => {
 router.get("/students", (req, res, next) => {
   const cityName = req.query.city;
   const cohortName = req.query.cohort;
-  if (cityName && cohortName == undefined) {
-    connection.query(
-      "SELECT * FROM students WHERE lower(city) LIKE $1 || '%'",
+  let cityId;
+
+  if (cityName) {
+    Connection.query(
+      "SELECT id FROM cities WHERE cities_name = $1",
       [cityName],
       (err, result) => {
         if (err) {
-          return next(err);
+          res.status(500).json(err);
+        } else {
+          cityId = result.rows[0].id;
+          Connection.query(
+            "SELECT * FROM users WHERE city_id = $1 AND user_type ='student'",
+            [cityId],
+            (err, result) => {
+              if (err) {
+                res.status(500).json(err);
+              } else {
+                res.status(200).json(result.rows);
+              }
+            }
+          );
         }
-        res.status(200).json(result.rows);
       }
     );
-  } else if (cityName) {
-    if (cohortName) {
-      connection.query(
-        "SELECT * FROM students WHERE lower(city) LIKE $1 || '%' and lower(cohort) LIKE $2 || '%'",
-        [cityName, cohortName],
-        (err, result) => {
-          if (err) {
-            return next(err);
-          }
+  } else {
+    Connection.query(
+      "SELECT * FROM users WHERE user_type = 'student'",
+      (err, result) => {
+        if (err) {
+          res.status(500).json(err);
+        } else {
           res.status(200).json(result.rows);
         }
-      );
-    }
-  } else {
-    connection.query("SELECT * FROM students", (err, result) => {
-      if (err) {
-        return next(err);
       }
-      res.status(200).json(result);
-    });
+    );
   }
+
+  // if (cityName && cohortName == undefined) {
+  //   Connection.query(
+  //     "SELECT * FROM users WHERE lower(city) LIKE $1 || '%' and user_type = 'student'",
+  //     [cityName],
+  //     (err, result) => {
+  //       if (err) {
+  //         return next(err);
+  //       }
+  //       res.status(200).json(result.rows);
+  //     }
+  //   );
+  // } else if (cityName) {
+  //   if (cohortName) {
+  //     Connection.query(
+  //       "SELECT * FROM users WHERE lower(city) LIKE $1 || '%' and lower(cohort) LIKE $2 || '%' and user_type = 'student'",
+  //       [cityName, cohortName],
+  //       (err, result) => {
+  //         if (err) {
+  //           return next(err);
+  //         }
+  //         res.status(200).json(result.rows);
+  //       }
+  //     );
+  //   }
+  // } else {
+  //   Connection.query(
+  //     "SELECT * FROM users WHERE user_type = 'student'",
+  //     (err, result) => {
+  //       if (err) {
+  //         return next(err);
+  //       }
+  //       res.status(200).json(result);
+  //     }
+  //   );
+  // }
 });
 
+router.get("/cities", (req, res, nex) => {
+  Connection.query("SELECT cities_name FROM cities", (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(result.rows);
+    }
+  });
+});
 export default router;
--- server/db.js
+++ server/db.js
@@ -18,7 +18,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: "localhost",
     database: "compass",
-    password: "cyf2020",
+    password: "*********",
     port: 5432,
   };
 }
