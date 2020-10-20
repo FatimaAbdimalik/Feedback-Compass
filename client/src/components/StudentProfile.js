53add8966ab2760fe80e21ae5d6330bb4bb92a1a
Sulaiman <sulaimansolo73505@gmail.com>
2020-10-20 21:21:25 +0100

    added lanading page and student profile

--- client/src/components/StudentProfile.js
+++ client/src/components/StudentProfile.js
@@ -1,9 +1,158 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "./Logo.png"
+import axios from "axios";
+import avatar from "./Avatar.png"
+import "./StudentProfile.css";
+
+
+
+
+
 
 function StudentProfile() {
+
+  const [profilePhoto, setProfilePhto] = useState(avatar)
+const [studentDetails, setStudetDetails] = useState({
+  name: 'Sulaiman',
+  surname: 'Alhalabi'
+})
+
+
+
+
+
+////-----------Biography Section------------------>
+  const [bio, setBio] = useState('about you...')
+  const [submitBio, setSubmiBio] = useState('')
+  
+  const handleBioSubmit = (e)=> {
+    e.preventDefault();
+    setBio(submitBio)
+    document.getElementById('student-bio').value = ''
+  }
+  // axios
+  // .get("/api/users", {
+  // })
+  // .then(function (response) {
+  //   if (response) {
+     
+  //   }
+  // })
+  // .catch(function (error) {
+  //   if (error) {
+    
+  //   }
+  // });
+
+  //------------ Modules list  handling -------->
+const [moduleTitle, setModuleTitle] = useState('')
+  const [comment, setComment] = useState('')
+  const [isCommented, setIsCommented] = useState('')
+const handleComentBtn = (e)=>{
+  e.preventDefault();
+    setIsCommented(comment)
+    document.getElementById('comment-input').value = ''
+}
+
+
+
   return (
     <div>
-      <h1>Welcome to your profile</h1>
+     <div id = "student-container">
+          <div id = "student-heading">
+              <img id = "logo" src= {Logo} width="210" height="150"/>
+              <h1 className='welcom' >Track Your Feedback</h1>
+          </div>
+          <div id= 'student-body'>
+            <div id = 'student-profile'>
+              <img src={profilePhoto} id='avatar' />
+              <a href= '#'>Add a profile</a>
+              <h4>{studentDetails.name} {studentDetails.surname}</h4>
+                <h5 >{bio}</h5>
+              <input id = 'student-bio'
+               placeholder= 'add your biography'
+               type="text"
+               name="bio"
+               onChange={(e)=>{ setSubmiBio(e.target.value)}}
+                />
+              <button id='modules' onClick={handleBioSubmit}>save</button>
+              <div id= 'modules-container'>
+                <h2>Modules</h2>
+                  <select id='modules' name = 'HTML' onChange={(e)=>setModuleTitle(e.target.value + ' ' + e.target.name)}>
+                    <option>HTML</option>
+                    <option>WEEK-1</option>
+                    <option>WEEK-2</option>
+                    <option>WEEK-3</option>
+                  </select>
+
+                    <select id='modules' name= 'CSS' onChange={(e)=>setModuleTitle(e.target.value + ' ' + e.target.name)}>
+                      <option>CSS</option>
+                      <option>WEEK-1</option>
+                      <option>WEEK-2</option>
+                      <option>WEEK-3</option>
+                    </select>
+
+
+                    <select id='modules' name = 'Javascript' onChange={(e)=>setModuleTitle(e.target.value + ' ' + e.target.name)}>
+                      <option>Javascript</option>
+                      <option>WEEK-1</option>
+                      <option>WEEK-2</option>
+                      <option>WEEK-3</option>
+                    </select>
+
+
+
+                    <select id='modules' name = 'React' onChange={(e)=>setModuleTitle(e.target.value + ' ' + e.target.name)}>
+                      <option>React</option>
+                      <option>WEEK-1</option>
+                      <option>WEEK-2</option>
+                      <option>WEEK-3</option>
+                    </select>
+
+
+
+                    <select id='modules' name = 'Node.js' onChange={(e)=>setModuleTitle(e.target.value + ' ' + e.target.name)} >
+                      <option>Node.js</option>
+                      <option>WEEK-1</option>
+                      <option>WEEK-2</option>
+                      <option>WEEK-3</option>
+                    </select>
+
+
+                  <select id='modules' name = 'SQL' onChange={(e)=>setModuleTitle(e.target.value + ' ' + e.target.name)}>
+                    <option>SQL</option>
+                    <option>WEEK-1</option>
+                    <option>WEEK-2</option>
+                    <option>WEEK-3</option>
+                  </select>
+              </div>
+            </div>
+          
+          <div id='feedback'>
+            <div>
+              <h1>{moduleTitle} Feedback</h1>
+              <p>background and height as shown in the GitHub gist above. Since our list has a background of white we changed the Main background so that we could clearly see our selection.
+              </p>
+              <div id = 'comment'>
+                 <p>{isCommented}</p>
+              <input id = 'comment-input'
+               placeholder= 'write a comment'
+               type="text"
+               name="comment"
+               onChange={(e)=>{ setComment(e.target.value)}}
+                />
+                <div id= 'buttons'>
+                  <button  id='comment-btn' onClick={handleComentBtn}>Comment</button>
+                  <button  id='comment-btn'>Edit comment</button>
+                  <button  id='comment-btn'>Delete comment</button>
+                </div>
+              
+              </div>
+             
+            </div>
+          </div>
+          </div>
+        </div>
     </div>
   );
 }
