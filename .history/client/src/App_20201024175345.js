bce3571e38730bebbd15840069d56dc5825c2363
Leida_Gandy <leida.gandy@gmail.com>
2020-10-25 12:10:00 +0000

    Added login changes

--- .history/client/src/App_20201024175345.js
+++ .history/client/src/App_20201024175345.js
@@ -1,10 +1,11 @@
+
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import LandingPage from "./components/LandingPage";
 import StudentProfile from "./components/StudentProfile";
 import ChooseCohort from "./components/ChooseCohort";
-import MentorFeedback from "./components/MentorFeedback";
+import MentorFeedback from "./components/MentorFeedback"
 import "./App.css";
 
 function App() {
@@ -14,10 +15,8 @@ function App() {
       <Route path="/login" component={Homepage} />
       <Route path="/student" component={StudentProfile} />
       <Route path="/cohorts" component={ChooseCohort} />
-      <Route
-        path="/feedback/:student_id/:mentor_id"
-        component={MentorFeedback}
-      />
+      <Route path="/feedback/:student_id/:mentor_id" component={MentorFeedback} />
+
     </Router>
   );
 }
