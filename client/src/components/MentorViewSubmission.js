9ccfa63ceb16af1f72ae5dde74fe1888bdf8e1fe
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-31 20:16:01 +0000

    added according to mentor's view page

--- client/src/components/MentorViewSubmission.js
+++ client/src/components/MentorViewSubmission.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
+import { Accordion, Button, Card } from "react-bootstrap";
+
 import "./StudentProfile.css";
 import axios from "axios";
 import moment from "moment";
@@ -71,53 +72,116 @@ const SubmissionCard = ({ student_id, mentor_id }) => {
       {cardData.map((card, index) => {
         return (
           <div>
-            <Card.Body className="submission-card" key={index}>
-              <Card.Title id="card-title">{card.title}</Card.Title>
-              <div className="card-color">
-                <div id="card-date">
-                  Sent: {handleDate(card.submission_date)}
-                </div>
-                <div>
-                  <span>
-                    <a
-                      className="submission-link"
-                      href={card.submission}
-                      target="_blank"
-                    >
-                      {card.submission}{" "}
-                    </a>
-                  </span>
-                </div>
-                <div>{card.body}</div>
+            <Accordion>
+              <Card className="submission-card" key={index}>
+                <Card.Title
+                  id="card-title"
+                  style={{ width: "40rem", display: "flex" }}
+                >
+                  <Accordion.Toggle as={Button} variant="light" eventKey="0">
+                    {card.title}
+                  </Accordion.Toggle>
+                </Card.Title>
+                <Accordion.Collapse eventKey="0">
+                  <div className="card-color">
+                    <div id="card-date">
+                      Sent: {handleDate(card.submission_date)}
+                    </div>
+                    <div>
+                      <span>
+                        <a
+                          className="submission-link"
+                          href={card.submission}
+                          target="_blank"
+                        >
+                          {card.submission}{" "}
+                        </a>
+                      </span>
+                    </div>
+                    <div>{card.body}</div>
 
-                <div id="comment">
-                  <input
-                    className="comment-input"
-                    id={"input" + card.id}
-                    placeholder="write a feedback"
-                    type="text"
-                    name="comment"
-                    onChange={handleChange}
-                  />
-                  <div id="buttons">
-                    <button
-                      id="comment-btn"
-                      value={card.id}
-                      onClick={handleSubmitFeedback}
-                    >
-                      Submit Feedback
-                    </button>
-                    <button id="comment-btn">Edit Feedback</button>
-                    <button id="comment-btn">Delete Feedback</button>
+                    <div id="comment">
+                      <input
+                        className="comment-input"
+                        id={"input" + card.id}
+                        placeholder="write a feedback"
+                        type="text"
+                        name="comment"
+                        onChange={handleChange}
+                      />
+                      <div id="buttons">
+                        <button
+                          id="comment-btn"
+                          value={card.id}
+                          onClick={handleSubmitFeedback}
+                        >
+                          Submit Feedback
+                        </button>
+                        <button id="comment-btn">Edit Feedback</button>
+                        <button id="comment-btn">Delete Feedback</button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
           </div>
         );
       })}
     </div>
   );
 };
+
+{
+  /* <Accordion>
+<Card className="submission-card" key={index}>
+  <Card.Title style={{ width: "40rem", display: "flex" }}>
+    <Accordion.Toggle as={Button} variant="light" eventKey="0">
+      {" "}
+      {card.title}
+    </Accordion.Toggle>
+  </Card.Title>
+  <Accordion.Collapse eventKey="0">
+    <div className="card-color">
+      <div id="card-date">
+        {" "}
+        Sent: {handleDate(card.submission_date)}
+      </div>
+      <div id="card-submitted">
+        <h5>Submitted Work:</h5>{" "}
+        <span>
+          <a
+            className="submission-link"
+            href={card.submission}
+            target="_blank"
+          >
+            {card.submission}{" "}
+          </a>
+        </span>
+        <br />
+      </div>
+      <span>
+        {" "}
+        <div id="card-feedback"> {card.body}</div>
+      </span>
+
+      <div id="comment">
+        <input
+          id="comment-input"
+          placeholder="write a comment"
+          type="text"
+          name="comment"
+        />
+        <div id="buttons">
+          <button id="comment-btn">Submit comment</button>
+          <button id="comment-btn">Edit comment</button>
+          <button id="comment-btn">Delete comment</button>
+        </div>
+      </div>
+    </div>
+  </Accordion.Collapse>
+</Card>
+</Accordion> */
+}
 
 export default SubmissionCard;
