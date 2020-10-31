ebf2e329e5795df4bde0d5ee49dc2c36cec12bb9
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-31 17:31:36 +0000

    added according

--- client/src/components/SubmissionCard.js
+++ client/src/components/SubmissionCard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
+import { Accordion, Button } from "react-bootstrap";
 import "./StudentProfile.css";
 import axios from "axios";
 
@@ -24,50 +25,71 @@ const SubmissionCard = ({ id }) => {
     <div>
       {cardData.map((card, index) => {
         return (
-          <Card.Body className="submission-card" key={index}>
-            <Card.Title id="card-title"> {card.title}</Card.Title>
-            <div className="card-color">
-              <div id="card-date">
-                {" "}
-                Sent: {handleDate(card.submission_date)}
-              </div>
-              <div id="card-submitted">
-                <h5>Submitted Work:</h5>{" "}
-                <span>
-                  <a
-                    className="submission-link"
-                    href={card.submission}
-                    target="_blank"
-                  >
-                    {card.submission}{" "}
-                  </a>
-                </span>
-                <br />
-              </div>
-              <span>
-                {" "}
-                <div id="card-feedback"> {card.body}</div>
-              </span>
+          <Accordion>
+            <Card className="submission-card" key={index}>
+              <Card.Title style={{ width: "40rem" }}>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  {" "}
+                  {card.title}
+                </Accordion.Toggle>
+              </Card.Title>
+              <Accordion.Collapse eventKey="0">
+                <div className="card-color">
+                  <div id="card-date">
+                    {" "}
+                    Sent: {handleDate(card.submission_date)}
+                  </div>
+                  <div id="card-submitted">
+                    <h5>Submitted Work:</h5>{" "}
+                    <span>
+                      <a
+                        className="submission-link"
+                        href={card.submission}
+                        target="_blank"
+                      >
+                        {card.submission}{" "}
+                      </a>
+                    </span>
+                    <br />
+                  </div>
+                  <span>
+                    {" "}
+                    <div id="card-feedback"> {card.body}</div>
+                  </span>
 
-              <div id="comment">
-                <input
-                  id="comment-input"
-                  placeholder="write a comment"
-                  type="text"
-                  name="comment"
-                />
-                <div id="buttons">
-                  <button id="comment-btn">Submit comment</button>
-                  <button id="comment-btn">Edit comment</button>
-                  <button id="comment-btn">Delete comment</button>
+                  <div id="comment">
+                    <input
+                      id="comment-input"
+                      placeholder="write a comment"
+                      type="text"
+                      name="comment"
+                    />
+                    <div id="buttons">
+                      <button id="comment-btn">Submit comment</button>
+                      <button id="comment-btn">Edit comment</button>
+                      <button id="comment-btn">Delete comment</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         );
       })}
     </div>
   );
 };
 
+///<Accordion>
+//   <Card>
+//   <Card.Header>
+//     <Accordion.Toggle as={Button} variant="link" eventKey="0">
+//       Click me!
+//     </Accordion.Toggle>
+//   </Card.Header>
+//   <Accordion.Collapse eventKey="0">
+//     <Card.Body>Hello! I'm the body</Card.Body>
+//   </Accordion.Collapse>
+// </Card>
+// </Accordion>
 export default SubmissionCard;
